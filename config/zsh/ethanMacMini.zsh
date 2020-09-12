941b748d2f207491f57cc9e62ff7a41c96160baa
Ethan Rietz <ewrietz@gmail.com>
2020-09-12 16:55:01 -0400

    get conda to work on macmini

---
+++ config/zsh/ethanMacMini.zsh
@@ -0,0 +1,19 @@
+export PATH="$PATH:/Applications/MATLAB_R2020a.app/bin/matlab"
+
+# Created using /usr/local/anaconda3/bin/conda init zsh
+#   - Then copy the content of ~/.zshrc to here
+#
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/usr/local/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/usr/local/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
