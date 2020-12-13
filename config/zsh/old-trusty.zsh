878fb597863ed6a059dfff5e5f65c3ab46a621c3
Ethan Rietz <ewrietz@gmail.com>
2020-12-13 15:55:17 -0600

    tweaks

--- config/zsh/old-trusty.zsh
+++ config/zsh/old-trusty.zsh
@@ -1,3 +1,4 @@
 alias \
-	ls="ls --color=auto"
-	spotify="spotify --force-device-scale-factor=2"
+	ls="ls --color=auto" \
+	spotify="spotify --force-device-scale-factor=2 &> /dev/null & disown" \
+	keybindings='grep "bindsym \$mod+" .config/i3/config | fzf' \
