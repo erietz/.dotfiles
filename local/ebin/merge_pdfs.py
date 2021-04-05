b9ec9e206624ce28f77a530bd0e58d216d508093
Ethan Rietz <ewrietz@gmail.com>
2021-04-05 14:07:44 -0500

    cleaner code for colors

--- local/ebin/merge_pdfs.py
+++ local/ebin/merge_pdfs.py
@@ -3,25 +3,22 @@
 import sys
 from PyPDF2 import PdfFileMerger
 
-def colored(string, color):
-    colors = dict(
-        HEADER = '\033[95m',
-        OKBLUE = '\033[94m',
-        OKCYAN = '\033[96m',
-        OKGREEN = '\033[92m',
-        WARNING = '\033[93m',
-        FAIL = '\033[91m',
-        ENDC = '\033[0m',
-        BOLD = '\033[1m',
-        UNDERLINE = '\033[4m',
-    )
-    return f'{colors[color]}{string}{colors["ENDC"]}'
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 if len(sys.argv) < 3:
-    raise ValueError('Usage: ' + colored(f"{sys.argv[0]} <file1.pdf> <file2.pdf> <output_file.pdf>", 'OKBLUE'))
+    raise ValueError(f'Usage: {bcolors.OKBLUE}{sys.argv[0]} <file1.pdf> <file2.pdf> <output_file.pdf>{bcolors.ENDC}') 
 
 
-print(f'You are about to ' + colored("CLOBBER", 'FAIL') + f' any files named {sys.argv[-1]}')
+print(f'You are about to {bcolors.FAIL}CLOBBER{bcolors.ENDC} any files named {sys.argv[-1]}')
 proceed = input('Do you want to continue? [y/n] ')
 if proceed.lower() != 'y':
     sys.exit('Aborted by user')
