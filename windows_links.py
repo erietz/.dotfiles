368b7cc0d481945a5702345c46467c8a9877162b
Ethan Rietz <ewrietz@gmail.com>
2021-04-16 08:14:47 -0500

    changing windows link script

--- windows_links.py
+++ windows_links.py
@@ -1,53 +1,55 @@
 #!/usr/bin/env python3
-
 #-------------------------------------------------------------------------------
 # Author: Ethan Rietz
-# Date: Wed Apr 14 13:59:12 CDT 2021
-# Description:
-#   Create symlinks to Windows directories from WSL directories
-#   Note: Must be run from command prompt as administrator
+# Date: 04/16/2021
+# Description: Symlink my dotfiles manually on Windows
 #-------------------------------------------------------------------------------
 
-from pathlib import Path, WindowsPath
-import subprocess
+import sys
+import os
+from pathlib import WindowsPath
 
-path_pairs = [
-    (
-        '/mnt/c/Users/ewrie/AppData/Local/nvim/',
-        '/home/ethan/.ewr/config/nvim/'
-    ),
-    (
-        '/mnt/c/Users/ewrie/AppData/Roaming/alacritty/',
-        '/home/ethan/.ewr/config/alacritty/'
-    ),
+if sys.argv[1] == 'for-real':
+    DRY_RUN=False
+else:
+    DRY_RUN=True
+    print('------------------------------DRY RUN------------------------------')
+
+PATH_PAIRS = [
+    {
+        'git_file': 'C:/Users/ewrie/git/.ewr/config/nvim/',
+        'win_file': 'C:/Users/ewrie/AppData/Local/nvim/',
+    },
+    {
+        'git_file': 'C:/Users/ewrie/git/.ewr/config/alacritty/',
+        'win_file': 'C:/Users/ewrie/AppData/Roaming/alacritty/',
+    },
 ]
 
-def sanitize_path_pairs(path_pairs):
+def sanitize_paths(path_pairs):
     new_pairs = []
     for pair in path_pairs:
-        dos_path = Path(pair[0])
-        wsl_path = Path(pair[1])
-        dos_path = Path(*dos_path.parts[3:])
-        dos_path = WindowsPath('C:\\') / WindowsPath(dos_path)
-        wsl_path = WindowsPath('\\\\wsl$\\Ubuntu') / WindowsPath(wsl_path)
-        new_pairs.append([dos_path, wsl_path])
+        new_dict = {}
+        for key,value in pair.items():
+            new_dict[key] = WindowsPath(value)
+        new_pairs.append(new_dict)
     return new_pairs
 
-def create_links(cleansed_path_pairs):
-    for pair in cleansed_path_pairs:
-        if not pair[0].exists():
-            if pair[1].is_dir():
-                cmd = f'mklink /D {pair[0]} {pair[1]}'.split()
+def create_links(path_pairs, dry_run=True):
+    fail = '\033[91m'
+    endc = '\033[0m'
+    for pair in path_pairs:
+        win_file = pair['win_file']
+        git_file = pair['git_file']
+        if not win_file.exists() and git_file.exists():
+            if dry_run:
+                print('Create link', win_file, '--->', git_file)
             else:
-                cmd = f'mklink {pair[0]} {pair[1]}'.split()
-            print('Running this command: ',  ' '.join(cmd))
-            subprocess.run(['cmd.exe', '/c'] + cmd, shell=True)
+                os.symlink(git_file, win_file)
         else:
-            print(pair[0], 'already exists .... no link created')
+            print(f'{fail}Already exists...{win_file}...no link created{endc}')
+    return None
 
-cleansed_path_pairs = sanitize_path_pairs(path_pairs)
-
-create_links(cleansed_path_pairs)
-
-#for i in cleansed_path_pairs:
-#    print(i)
+if __name__ == '__main__':
+    clean_path_pairs = sanitize_paths(PATH_PAIRS)
+    create_links(clean_path_pairs, dry_run=DRY_RUN)
