70f3911e0ef5f1c5f5245b434bc5463bfc762f43
Ethan Rietz <ewrietz@gmail.com>
2021-11-09 14:10:23 -0600

    more consistent parenthesis

--- config/nvim/lua/ewr/colors.lua
+++ config/nvim/lua/ewr/colors.lua
@@ -2,9 +2,9 @@ vim.opt.termguicolors = true    -- Better colors
 vim.opt.background = "light"    -- Sets theme below
 
 if vim.api.nvim_get_option("background") == "dark" then
-    vim.cmd([[colorscheme base16-gruvbox-dark-soft]])
-    vim.env.BAT_THEME = 'gruvbox-dark'
+    vim.cmd("colorscheme base16-gruvbox-dark-soft")
+    vim.env.BAT_THEME = "gruvbox-dark"
 else
-    vim.cmd([[colorscheme base16-solarized-light]])
-    vim.env.BAT_THEME = 'Solarized (light)'
+    vim.cmd("colorscheme base16-solarized-light")
+    vim.env.BAT_THEME = "Solarized (light)"
 end
