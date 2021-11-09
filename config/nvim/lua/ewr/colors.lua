c52d3d10eefaabb9e9f7ff5af4eb01a4363abcf6
Ethan Rietz <ewrietz@gmail.com>
2021-11-09 14:08:51 -0600

    fix bat preview colors

--- config/nvim/lua/ewr/colors.lua
+++ config/nvim/lua/ewr/colors.lua
@@ -1,6 +1,10 @@
 vim.opt.termguicolors = true    -- Better colors
-vim.opt.background = "dark"     -- Dark theme
+vim.opt.background = "light"    -- Sets theme below
 
-vim.cmd([[colorscheme gruvbox]])
--- vim.cmd([[colorscheme seoul256]])
---require('colorbuddy').colorscheme('Set3')
+if vim.api.nvim_get_option("background") == "dark" then
+    vim.cmd([[colorscheme base16-gruvbox-dark-soft]])
+    vim.env.BAT_THEME = 'gruvbox-dark'
+else
+    vim.cmd([[colorscheme base16-solarized-light]])
+    vim.env.BAT_THEME = 'Solarized (light)'
+end
