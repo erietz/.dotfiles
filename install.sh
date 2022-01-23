311eddd4678210bd0d8f364a1b4fc0b424e912c0
Ethan Rietz <ewrietz@gmail.com>
2022-01-23 16:27:04 -0500

    Move to GNU Stow

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+os=$(/bin/uname -s)
+
+stow_it() {
+    stow -v $1
+}
+
+case "$os" in
+    Linux) stow_it linux;;
+    Darwin) stow_it darwin;;
+    *) echo "unknown os" && exit 1;;
+esac
+
+stow_it unix
