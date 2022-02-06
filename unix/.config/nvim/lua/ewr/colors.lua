7698e89c5f8c0e233a4063416989a890c3773bb7
Ethan Rietz <ewrietz@gmail.com>
2022-02-06 17:42:09 -0600

    minor changes

--- unix/.config/nvim/lua/ewr/colors.lua
+++ unix/.config/nvim/lua/ewr/colors.lua
@@ -1,5 +1,5 @@
 vim.opt.termguicolors = true    -- Better colors
-vim.opt.background = "light"    -- Sets theme below
+vim.opt.background = "dark"    -- Sets theme below
 
 -- vim.env.BAT_THEME = "ansi"
 if vim.api.nvim_get_option("background") == "dark" then
