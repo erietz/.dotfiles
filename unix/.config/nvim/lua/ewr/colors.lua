eb354e85c13ed552c6525eb6f3bcea1e60874828
Ethan Rietz <ewrietz@gmail.com>
2022-05-16 22:07:18 -0500

    git stash does not fix all problems

--- unix/.config/nvim/lua/ewr/colors.lua
+++ unix/.config/nvim/lua/ewr/colors.lua
@@ -3,8 +3,10 @@ vim.opt.background = "dark"    -- Sets theme below
 
 -- vim.env.BAT_THEME = "ansi"
 if vim.api.nvim_get_option("background") == "dark" then
-    vim.g.gruvbox_contrast_dark = "soft"
+    -- vim.g.gruvbox_contrast_dark = "soft"
     vim.cmd("colorscheme gruvbox")
+    -- vim.g.seoul256_background = 236
+    -- vim.cmd("colorscheme seoul256")
 else
     vim.cmd("colorscheme PaperColor")
 end
