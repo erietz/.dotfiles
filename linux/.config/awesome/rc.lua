f8f79f2f3e20dbd0696f1a3ddb40025cc2a0ccb6
Ethan Rietz <ewrietz@gmail.com>
2022-07-06 18:04:18 -0500

    dont know why i had vicious

--- linux/.config/awesome/rc.lua
+++ linux/.config/awesome/rc.lua
@@ -20,7 +20,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
-local vicious = require("vicious")
 
 -- Error handling {{{
 -- Check if awesome encountered an error during startup and fell back to
