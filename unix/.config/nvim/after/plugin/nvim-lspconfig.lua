f6064fca5a821f8171b667cfef3d4270345fa7da
erietz <ewrietz@gmail.com>
2022-10-25 13:26:47 -0500

    tweaking

--- unix/.config/nvim/after/plugin/nvim-lspconfig.lua
+++ unix/.config/nvim/after/plugin/nvim-lspconfig.lua
@@ -1,56 +1,57 @@
 local lspconfig = require("lspconfig")
+local util = require("lspconfig").util
 local keymap = require("ewr.keymap")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { silent = true, buffer = bufnr }
-  keymap.nnoremap('gD', vim.lsp.buf.declaration, bufopts)
-  keymap.nnoremap('gd', vim.lsp.buf.definition, bufopts)
-  keymap.nnoremap('K', vim.lsp.buf.hover, bufopts)
-  keymap.nnoremap('gi', vim.lsp.buf.implementation, bufopts)
-  keymap.nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts)
-  keymap.nnoremap('<C-j>', vim.lsp.buf.hover, bufopts)
-  keymap.nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  keymap.nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  keymap.nnoremap('<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  keymap.nnoremap('<space>D', vim.lsp.buf.type_definition, bufopts)
-  keymap.nnoremap('<space>rn', vim.lsp.buf.rename, bufopts)
-  keymap.nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts)
-  keymap.nnoremap('gr', vim.lsp.buf.references, bufopts)
-  keymap.nnoremap('<space>fm', vim.lsp.buf.formatting, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { silent = true, buffer = bufnr }
+    keymap.nnoremap('gd', vim.lsp.buf.definition, bufopts)
+    keymap.nnoremap('gD', vim.lsp.buf.declaration, bufopts)
+    keymap.nnoremap('gr', vim.lsp.buf.references, bufopts)
+    keymap.nnoremap('K', vim.lsp.buf.hover, bufopts)
+    keymap.nnoremap('gi', vim.lsp.buf.implementation, bufopts)
+    keymap.nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts)
+    keymap.nnoremap('<C-j>', vim.lsp.buf.hover, bufopts)
+    keymap.nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    keymap.nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    keymap.nnoremap('<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    keymap.nnoremap('<space>D', vim.lsp.buf.type_definition, bufopts)
+    keymap.nnoremap('<space>rn', vim.lsp.buf.rename, bufopts)
+    keymap.nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts)
+    keymap.nnoremap('<space>fm', vim.lsp.buf.format, bufopts)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
+    vim.lsp.protocol.make_client_capabilities()
 )
 
 lspconfig['sumneko_lua'].setup({
-  -- cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-        },
-      },
+    -- cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                },
+            },
+        }
     }
-  }
 })
 
 lspconfig['pylsp'].setup({
@@ -59,45 +60,46 @@ lspconfig['pylsp'].setup({
 })
 
 lspconfig['rust_analyzer'].setup({
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 })
 
 lspconfig['tsserver'].setup({
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 })
 
 lspconfig['clangd'].setup({
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 })
 
 lspconfig['csharp_ls'].setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "cs", "solution", "csproj" },
-  handlers = {
-    ["textDocument/definition"] = require("csharpls_extended").handler,
-  },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "cs" },
+    root_dir = util.root_pattern("*.sln") or util.root_pattern("*.csproj"),
+    handlers = {
+        ["textDocument/definition"] = require("csharpls_extended").handler,
+    },
 })
 
 lspconfig['html'].setup({
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 })
 
 lspconfig['cssls'].setup({
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 })
 
 lspconfig['dartls'].setup({
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 })
 
 lspconfig.gopls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
+    on_attach = on_attach,
+    capabilities = capabilities
 })
