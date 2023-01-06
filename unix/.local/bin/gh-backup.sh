d99d35dee9b307e8b28ea7ae9fa1ac8b96e46c20
erietz <ewrietz@gmail.com>
2023-01-06 17:17:29 -0600

    improve shell script

--- unix/.local/bin/gh-backup.sh
+++ unix/.local/bin/gh-backup.sh
@@ -6,18 +6,19 @@
 #-------------------------------------------------------------------------------
 
 set -x
-BACKUP_DIR="${HOME}/gh_backup"
-
+BACKUP_DIR="${HOME}/cron/gh_backup"
+MAX_REPOS=1000
+GITHUB_HANDLE="erietz"
 
 mkdir -p "$BACKUP_DIR"
-gh repo list erietz --limit 1000 | while read -r repo _; do
-	repo_name=$(echo $repo | sed 's/erietz\///')
-	outdir="${BACKUP_DIR}/$repo_name"
-	gh repo clone "$repo" "$outdir" -- -q 2>/dev/null || (
-		cd "$outdir"
-		# Handle case where local checkout is on a non-main/master branch
-		# - ignore checkout errors because some repos may have zero commits, 
-		# so no main or master
+
+gh repo list "$GITHUB_HANDLE" --limit "$MAX_REPOS" | cut -d '/' -f2- | while read -r repo_name _; do
+	out_dir="${BACKUP_DIR}/$repo_name"
+	gh repo clone "$repo" "$out_dir" -- -q 2>/dev/null || (
+		cd "$out_dir"
+		# Handle case where local checkout is on a non-main/master branch.
+		# Ignore checkout errors because some repos may have zero commits, so
+		# no main or master.
 		git checkout -q master 2>/dev/null || true
 		git checkout -q main 2>/dev/null || true
 		git pull -q
