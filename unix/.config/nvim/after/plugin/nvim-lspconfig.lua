b49cadd289868e934752e5e678b0801d9d6333c6
erietz <ewrietz@gmail.com>
2024-01-13 18:53:52 -0600

    macmini stashed changes

--- unix/.config/nvim/after/plugin/nvim-lspconfig.lua
+++ unix/.config/nvim/after/plugin/nvim-lspconfig.lua
@@ -125,14 +125,15 @@ lspconfig.texlab.setup(lsp_defaults({}))
 
 local null_ls = require("null-ls")
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.completion.spell,
-		null_ls.builtins.diagnostics.ruff,
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.isort,
-	},
+    sources = {
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.ruff,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.stylua,
+    },
 })
 
 -- local rt = require("rust-tools")
