4693d9220745e5441d189a8b267ddcf7d89673ed
erietz <ewrietz@gmail.com>
2024-09-01 13:43:23 -0500

    remove obsolete scripts

--- darwin/.local/bin/moveWindowNumberAndFollowFocus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/dash
-
-space=$1
-
-[ "$space" -gt 0 ] || echo "Error: enter an integer" || exit 0
-
-curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
-
-yabai -m window --space "$space" && yabai -m window --focus "$curWindowId"
