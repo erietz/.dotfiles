d0dda86074d4d23bb7327389efc39621126ae8f1
Ethan Rietz <ethan@peg.com>
2026-09-21 14:47:32 -0500

    do i need to untrack this file?

--- unix/.config/fish/conf.d/fish_frozen_theme.fish
+++ unix/.config/fish/conf.d/fish_frozen_theme.fish
@@ -1,33 +1,9 @@
-# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
-# To customize your theme, delete this file and see
-#     help interactive#syntax-highlighting
-# or
-#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
-# for appropriate commands to add to ~/.config/fish/config.fish instead.
-
-set --global fish_color_autosuggestion brblack
-set --global fish_color_cancel --reverse
-set --global fish_color_command normal
-set --global fish_color_comment red
-set --global fish_color_cwd green
-set --global fish_color_cwd_root red
-set --global fish_color_end green
-set --global fish_color_error brred
-set --global fish_color_escape brcyan
-set --global fish_color_history_current --bold
-set --global fish_color_host normal
-set --global fish_color_host_remote yellow
-set --global fish_color_normal normal
-set --global fish_color_operator brcyan
-set --global fish_color_param cyan
-set --global fish_color_quote yellow
-set --global fish_color_redirection cyan --bold
-set --global fish_color_search_match white --bold --background=brblack
-set --global fish_color_selection white --bold --background=brblack
-set --global fish_color_status red
-set --global fish_color_user brgreen
-set --global fish_color_valid_path --underline=single
-set --global fish_pager_color_description yellow --italics
-set --global fish_pager_color_prefix normal --bold --underline=single
-set --global fish_pager_color_progress brwhite --bold --background=cyan
-set --global fish_pager_color_selected_background --reverse
+# Source the isocon theme matching the current macOS appearance, and point
+# lazygit at the matching theme file layered on top of the user's own config.
+if defaults read -g AppleInterfaceStyle 2>/dev/null | string match -q Dark
+    source ~/.config/fish/themes/isocon-dark.fish
+    set -gx LG_CONFIG_FILE $HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/isocon-dark.yml
+else
+    source ~/.config/fish/themes/isocon-light.fish
+    set -gx LG_CONFIG_FILE $HOME/.config/lazygit/config.yml,$HOME/.config/lazygit/isocon-light.yml
+end
